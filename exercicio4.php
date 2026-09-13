1779501669a066faf34b60a0acb022099715398c
Gui <guilhermerafaell10@gmail.com>
2026-09-13 11:17:41 -0300

    desenvolvendo lógica do exercicio 4

--- exercicio4.php
+++ exercicio4.php
@@ -25,7 +25,25 @@
                 <input type="number" id="ano" name="ano" class="form-control" required="">
             </div>
             <button type="submit" class="btn btn-primary">Enviar</button>
-        </form>
+        </form><br>
+        <?php 
+        if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+        $dia = (int) $_POST['dia'];
+        $mes = (int) $_POST['mes'];
+        $ano = (int) $_POST['ano'];
+
+        if ($ano % 4 == 0){
+            if ($ano % 100 == 0){
+                if ($ano % 400 == 0){
+                    
+                }
+            }
+        }
+
+        }
+        
+        ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
     </div>
 </body>
