e28d69f3bff6b9d7044c953b5a6effad95d10e98
Gui <guilhermerafaell10@gmail.com>
2026-09-16 15:40:54 -0300

    implementando switch case para verificação das datas

--- exercicio4.php
+++ exercicio4.php
@@ -26,24 +26,44 @@
             </div>
             <button type="submit" class="btn btn-primary">Enviar</button>
         </form><br>
-        <?php 
-        if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-        $dia = (int) $_POST['dia'];
-        $mes = (int) $_POST['mes'];
-        $ano = (int) $_POST['ano'];
+            $dia = (int) $_POST['dia'];
+            $mes = (int) $_POST['mes'];
+            $ano = (int) $_POST['ano'];
 
-        if ($ano % 4 == 0){
-            if ($ano % 100 == 0){
-                if ($ano % 400 == 0){
-                    
+            if ($ano > 2026) {
+                echo "<h4>Data inválida</h4>";
+                echo "<h4>Motivo: Ano maior do que o atual (2026)</h4>";
+            } else {
+
+                $data = "$dia-$mes-$ano";
+                $data = date('d/m/Y', strtotime($data));
+
+                switch ($mes) {
+                    case '1':
+                        if ($dia > 31) {
+                            echo "<h4>Data inválida</h4>";
+                            echo "<h4>Motivo: Janeiro só possui 31 dias</h4>";
+                        } else {
+                            echo "<h4>Data correta</h4>";
+                            echo "<h4>$data</h4>";
+                        }
+                        break;
+
+                    default:
+                        # code...
+                        break;
                 }
             }
         }
 
-        }
-        
+
+
         ?>
+        <br>
+        <a href="index.html">Retornar ao início</a>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
     </div>
 </body>
