307bc4c10f593fa3feeae002790081360a6a7ab3
Gui <guilhermerafaell10@gmail.com>
2026-09-20 18:14:19 -0300

    exercicio 8 finalizado

--- exercicio8.php
+++ exercicio8.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercício 7 - Palavra Invertida</title>
+    <title>Exercício 8 - Vogais</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
@@ -22,10 +22,18 @@
         <?php
 
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $frase = (string) $_POST['frase'];            
+            $frase = $_POST['frase'];
             $frase = strtolower($frase);
 
-            echo preg_match_all()
+            $vogais = ['a', 'e', 'i', 'o', 'u'];
+            $contador = 0;
+
+            for ($i = 0; $i < strlen($frase); $i++) {
+                if (in_array($frase[$i], $vogais))
+                    $contador++;
+            }
+
+            echo "<h4>Quantidade de vogais: $contador</h4>";
         }
 
         ?>
